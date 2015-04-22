f9086c2f492e2cf393f00820f0ada8e5c7c5a1ba
Russell Murray <russellmurray@Russells-MacBook-Pro.local>
2015-04-22 17:46:25 -0400

    change karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -32,13 +32,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'src/*.js': 'coverage'
+    },
+
+    coverageReporter: {
+        type: 'cobertura',
+        dir: 'coverage/'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress','verbose'],
+    reporters: ['verbose','coverage'],
 
 
     // web server port
