b5b976c2d5d82ee99b38f7edf3f636a3648815e1
Ryan <Ryan@MBPs-MacBook-Pro.local>
2015-05-08 14:57:22 -0400

    added a final build task that combines the build process and bumpup

--- gruntfile.js
+++ gruntfile.js
@@ -38,5 +38,6 @@ module.exports = function(grunt) {
 	//Default task(s).
 	grunt.registerTask('build', ['karma:continuous', 'concat:noinfopath']);
 	grunt.registerTask('bump', ['bumpup']);
+	grunt.resisterTask('final', ['karma:continuous', 'concat:noinfopath', 'bumpup']);
 
 };
