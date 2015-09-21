7f93bda2cebb989fd4234aeccd6ec47625b8f13d
gochinj <jeff@imginconline.com>
2015-09-20 21:08:07 -0400

    Added code coverage back.

--- karma.conf.js
+++ karma.conf.js
@@ -43,21 +43,21 @@ module.exports = function(config) {
 
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-		// preprocessors: {
-		//     'src/*.js': 'coverage'
-		// },
-		//
-		// coverageReporter: {
-		//     type: 'cobertura',
-		//     dir: 'coverage/'
-		// },
+		preprocessors: {
+		    'src/*.js': 'coverage'
+		},
+
+		coverageReporter: {
+		    type: 'cobertura',
+		    dir: 'coverage/'
+		},
 
 
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		//reporters: ['verbose','coverage'],
-		reporters: ['verbose'],
+		reporters: ['verbose','coverage'],
+		// reporters: ['verbose'],
 
 
 		// web server port
